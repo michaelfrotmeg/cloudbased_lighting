de84b57c2a924e9e6867d60c1b33d01a57a04318
michaelfrotmeg <michael.frotmeg@outlook.com>
2025-03-22 14:21:46 +0100

    Add files via upload
    update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -27,7 +27,7 @@ exports.handler = async (event) => {
         let transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
+            secure: process.env.SMTP_PORT == 587, // true for 465, false for other ports
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
