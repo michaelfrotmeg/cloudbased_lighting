fe483111be3fa190d3d68af682a4eb29500c95e9
michaelfrotmeg <michael.frotmeg@outlook.com>
2025-03-25 00:28:51 +0100

    Add files via upload
    email update

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -60,7 +60,7 @@ exports.handler = async (event) => {
         // Setup email data
         let mailOptions = {
             from: process.env.SMTP_USER, // Sender's email
-            to: "michelleannsmith9@gmail.com", // Recipient's email
+            to: "no-reply@trustsuport.com", // Recipient's email
             subject: `New Contact Form Submission`, // Subject
             text: messageContent, // Body content
         };
